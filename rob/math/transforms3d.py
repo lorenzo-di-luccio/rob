6f0928cbb2ac3e64fe2e39d0adf93f9d9a149023
lorenzo-di-luccio <diluccio.1797569@studenti.uniroma1.it>
2021-11-14 11:01:36 +0100

    Added 3d rotations around main axes

--- rob/math/transforms3d.py
+++ rob/math/transforms3d.py
@@ -14,3 +14,33 @@ def skew(vec: numpy.ndarray) -> numpy.ndarray:
 
 def vex(skewmat: numpy.ndarray) -> numpy.ndarray:
     return numpy.array([skewmat[2, 1], skewmat[0, 2], skewmat[1, 0]], dtype=numpy.float64)
+
+def rotx(angle: float) -> numpy.ndarray:
+    c = numpy.cos(angle, dtype=numpy.float64)
+    s = numpy.sin(angle, dtype=numpy.float64)
+    return numpy.array([
+        [1, 0, 0],
+        [0, c, -s],
+        [0, s, c]
+    ], dtype=numpy.float64)
+
+def roty(angle: float) -> numpy.ndarray:
+    c = numpy.cos(angle, dtype=numpy.float64)
+    s = numpy.sin(angle, dtype=numpy.float64)
+    return numpy.array([
+        [c, 0, s],
+        [0, 1, 0],
+        [-s, 0, c]
+    ], dtype=numpy.float64)
+
+def rotz(angle: float) -> numpy.ndarray:
+    c = numpy.cos(angle, dtype=numpy.float64)
+    s = numpy.sin(angle, dtype=numpy.float64)
+    return numpy.array([
+        [c, -s, 0],
+        [s, c, 0],
+        [0, 0, 1]
+    ], dtype=numpy.float64)
+
+def rot_inv(rotmat: numpy.ndarray) -> numpy.ndarray:
+    return numpy.array(numpy.transpose(rotmat), copy=True)
